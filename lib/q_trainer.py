8f471c4fd0a62a8e3f93ffdbf90ca4ad81fa6fed
Andrew Lewin <andrew@lewin.org>
2018-07-22 00:19:58 -0600

    import debugging output. begin playing for black as well. we use a turn multiplier to make a move that's bad for white a positive value if it was black who made the move. we need to use nanargmax rather than nanargmin when playing for the other player since both players are maximizing their scores instead of having white maximize while black minimizes

--- lib/q_trainer.py
+++ lib/q_trainer.py
@@ -18,7 +18,7 @@ reward_list = []
 
 def get_reward(board, state, action, new_state, immediate_reward):
   if immediate_reward != 0: return immediate_reward
-  opponant_action_index = np.nanargmin(Q[new_state,:])
+  opponant_action_index = np.nanargmax(Q[new_state,:])
   opponant_move = generate_move_from_action(board, opponant_action_index)
   if opponant_move == None:
     invalid_action(new_state, opponant_action_index)
@@ -32,14 +32,15 @@ def get_reward(board, state, action, new_state, immediate_reward):
 
   return (1 - alpha) * Q[state,action] + alpha * learned
 
-def print_status_update(board, immediate_reward, before_reward, state, action, old_state):
+def print_status_update(board, immediate_reward, before_reward, reward, old_state, action):
   last_board_unicode = serializers.unicode(board)
   board.pop()
   penultimate_board_unicode = serializers.unicode(board)
   print(penultimate_board_unicode)
   print(last_board_unicode)
+  print("winner: " + ("black" if board.turn == chess.BLACK else "white"))
   print("immediate_reward: " + str(immediate_reward))
-  print("reward: " + str(before_reward) + " => " + str(Q[state,action]))
+  print("reward: (" + str(reward) + ") " + str(before_reward) + " => " + str(Q[old_state,action]))
   print(Q[old_state,:])
   print("reached destination!")
 
@@ -47,14 +48,18 @@ def invalid_action(state, action_index):
   Q[state, action_index] = np.nan
 
 def get_immediate_reward(board):
+  turn_multiplier = 1 if board.turn == chess.BLACK else -1
+
   r = 0
   if board.is_checkmate():
     r = 100
   if board.is_stalemate():
     r = -100
-  if len(list(board.pieces(chess.ROOK, chess.WHITE))) == 0:
-    r = -100
-  return r
+  if len(board.pieces(chess.ROOK, chess.WHITE)) == 0:
+    r = -100 # only two kings is an automatic stalemate
+  if len(board.pieces(chess.KING, chess.BLACK)) == 0:
+    r = 1 # we started with a bad board where black was already in check
+  return r * turn_multiplier
 
 def generate_move_from_action(board, action_index):
   action = satg.ACTIONS[action_index]
@@ -87,7 +92,6 @@ for i in range(num_episodes):
   reward_all = 0
   is_destination = False
   state = q_lookup[satg.board_key(board)]
-  print("----------\nnew game\n----------")
   for j in range(1000):
     move, action = get_valid_move(board, state)
     board.push(move)
@@ -98,12 +102,13 @@ for i in range(num_episodes):
     new_state = q_lookup[satg.board_key(board)]
 
     before_reward = Q[state,action]
-    Q[state,action] = get_reward(board,state,action,new_state,immediate_reward)
+    reward = get_reward(board,state,action,new_state,immediate_reward)
+    Q[state,action] = reward
 
     reward_all += immediate_reward
     state = new_state
     if is_destination:
-      print_status_update(board, immediate_reward, before_reward, state, action, old_state)
+      print_status_update(board, immediate_reward, before_reward, reward, old_state, action)
       break
   reward_list.append(reward_all)
   print("Score over time: " +  str(sum(reward_list)/num_episodes))
