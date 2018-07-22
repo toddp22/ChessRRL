3abf4ad8066767c6b4b10cc5a4912166910f965f
Andrew Lewin <andrew@lewin.org>
2018-07-21 21:52:07 -0600

    rename single letter variables to full names

--- lib/q_trainer.py
+++ lib/q_trainer.py
@@ -14,74 +14,74 @@ num_episodes = 5_000_000
 
 reward_list = []
 
-def print_status_update(b, immediate_reward, before_reward, state, a, old_state):
-  last_board_unicode = serializers.unicode(b)
-  b.pop()
-  penultimate_board_unicode = serializers.unicode(b)
+def print_status_update(board, immediate_reward, before_reward, state, action, old_state):
+  last_board_unicode = serializers.unicode(board)
+  board.pop()
+  penultimate_board_unicode = serializers.unicode(board)
   print(penultimate_board_unicode)
   print(last_board_unicode)
   print("immediate_reward: " + str(immediate_reward))
-  print("reward: " + str(before_reward) + " => " + str(Q[state,a]))
+  print("reward: " + str(before_reward) + " => " + str(Q[state,action]))
   print(Q[old_state,:])
   print("reached destination!")
 
 def invalid_action(state, action_index):
   Q[state, action_index] = np.nan
 
-def get_immediate_reward(b):
+def get_immediate_reward(board):
   r = 0
-  if b.is_checkmate():
+  if board.is_checkmate():
     r = 100
-  if b.is_stalemate():
+  if board.is_stalemate():
     r = -100
-  if len(list(b.pieces(chess.ROOK, chess.WHITE))) == 0:
+  if len(list(board.pieces(chess.ROOK, chess.WHITE))) == 0:
     r = -100
   return r
 
-def get_valid_move(b, bk):
-  action_index = np.nanargmax(Q[bk,:] + np.random.randn(action_count)*(np.float64(2 * num_episodes) / np.float64(i+1)))
+def get_valid_move(board, state):
+  action_index = np.nanargmax(Q[state,:] + np.random.randn(action_count)*(np.float64(2 * num_episodes) / np.float64(i+1)))
   action = satg.ACTIONS[action_index]
   action_piece = action[0]
   action_direction = action[1]
-  action_square = list(b.pieces(action_piece.piece_type, action_piece.color))[0]
+  action_square = list(board.pieces(action_piece.piece_type, action_piece.color))[0]
   result_square = action_square + action_direction
 
   if result_square >= 64 or result_square < 0:
-    invalid_action(bk, action_index)
-    return get_valid_move(b, bk)
+    invalid_action(state, action_index)
+    return get_valid_move(board, state)
 
   move = chess.Move(action_square, result_square)
 
-  if move not in b.generate_legal_moves():
-    invalid_action(bk, action_index)
-    return get_valid_move(b, bk)
+  if move not in board.generate_legal_moves():
+    invalid_action(state, action_index)
+    return get_valid_move(board, state)
 
   return move, action_index
 
 np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 print("Begin!")
 for i in range(num_episodes):
-  b = generators.random_krk_board()
+  board = generators.random_krk_board()
   reward_all = 0
   is_destination = False
-  state = q_lookup[satg.board_key(b)]
+  state = q_lookup[satg.board_key(board)]
   print("----------\nnew game\n----------")
   for j in range(1000):
-    m,a = get_valid_move(b, state)
-    b.push(m)
-    immediate_reward = get_immediate_reward(b)
+    move, action = get_valid_move(board, state)
+    board.push(move)
+    immediate_reward = get_immediate_reward(board)
     is_destination = immediate_reward != 0
 
     old_state = state
-    new_state = q_lookup[satg.board_key(b)]
+    new_state = q_lookup[satg.board_key(board)]
 
-    before_reward = Q[state,a]
-    Q[state,a] = Q[state,a] + alpha * (immediate_reward + gamma * np.nanmax(Q[new_state,:]) - Q[state,a])
+    before_reward = Q[state,action]
+    Q[state,action] = Q[state,action] + alpha * (immediate_reward + gamma * np.nanmax(Q[new_state,:]) - Q[state,action])
 
     reward_all += immediate_reward
     state = new_state
     if is_destination:
-      print_status_update(b, immediate_reward, before_reward, state, a, old_state)
+      print_status_update(board, immediate_reward, before_reward, state, action, old_state)
       break
   reward_list.append(reward_all)
   print("Score over time: " +  str(sum(reward_list)/num_episodes))
