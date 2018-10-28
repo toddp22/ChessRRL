fecabd94d8d8c75b7e6bf976ffbbd7eed44ea3d9
Andrew Lewin <andrew@lewin.org>
2018-10-28 16:18:51 -0600

    have the q_learning agent play against itself

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import chess
 # custom modules
 from lib.board_operations import generators
 from lib.board_operations import serializers
-from lib.agents import minimax_agent as white
+from lib.agents import q_learning_agent as white
 from lib.agents import q_learning_agent as black
 
 board = generators.random_krk_board()
@@ -13,7 +13,7 @@ print("New game!")
 print(serializers.unicode(board))
 while (not board.is_game_over()):
   if board.turn == chess.WHITE:
-    move = white.get_move(board.copy(), depth=7)
+    move = white.get_move(board.copy())
   else:
     move = black.get_move(board.copy())
   board.push(move)
